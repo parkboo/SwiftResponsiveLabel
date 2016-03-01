ba0f46bd03715533a39be588a8c0072450e8bdc3
hsusmita <susmita.horrow@gmail.com>
2016-03-01 12:01:03 +0530

    Initial project added

---
+++ SwiftResponsiveLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftResponsiveLabel
+//
+//  Created by Susmita Horrow on 29/02/16.
+//  Copyright © 2016 hsusmita.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
