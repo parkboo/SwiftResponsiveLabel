6a4a422fcbbad71f1e8001540c1bd3797566386b
parkboo <parkboo@gmail.com>
2021-10-21 21:31:42 +0900

    Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftResponsiveLabel",
-            path: "Sources",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SwiftResponsiveLabelTests",
-            path: "Tests",
-            dependencies: ["SwiftResponsiveLabel"]),
+            dependencies: ["SwiftResponsiveLabel"],
+            path: "Tests"),
     ]
 )
