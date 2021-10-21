8a3fd1fef4f777119a072fd3970bf23ae0d4bfcf
parkboo <parkboo@gmail.com>
2021-10-21 21:57:39 +0900

    Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftResponsiveLabel",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,10 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftResponsiveLabel",
-            path: "SwiftResponsiveLabel"),
+            path: "Sources"),
         .testTarget(
             name: "SwiftResponsiveLabelTests",
             dependencies: ["SwiftResponsiveLabel"],
-            path: "SwiftResponsiveLabelTests"),
+            path: "Tests"),
     ]
 )
